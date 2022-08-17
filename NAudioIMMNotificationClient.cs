0cb296b429f4ef0dce4960236e8decd5b6d21783
Sannoob <cotwo0139@gmail.com>
2022-08-17 10:47:06 +0900

    Initial commit

---
+++ NAudioIMMNotificationClient.cs
@@ -0,0 +1,40 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+
+namespace DeviceVolume
+{
+    class NAudioIMMNotificationClient : NAudio.CoreAudioApi.Interfaces.IMMNotificationClient
+    {
+        public void OnDeviceStateChanged(string deviceId, DeviceState newState)
+        {
+            MMDevice mmDevice = Vault.deviceEnum.GetDevice(deviceId);
+            if (mmDevice.DataFlow != DataFlow.Render) return;
+            Console.WriteLine($"[DeviceStateChanged] {mmDevice.FriendlyName} {newState}");
+            if (mmDevice.DeviceFriendlyName == Vault.deviceName && newState == DeviceState.Active)
+            {
+                Console.WriteLine($"[NAudioIMMNotificationClient] Device Plugged, change volume to {(int)(Vault.defaultVolume * 100)}");
+                mmDevice.AudioEndpointVolume.MasterVolumeLevelScalar = Vault.defaultVolume;
+            }
+        }
+        public void OnDeviceAdded(string pwstrDeviceId)
+        {
+            return;
+        }
+
+        public void OnDeviceRemoved(string deviceId)
+        {
+            return;
+        }
+
+        public void OnDefaultDeviceChanged(DataFlow flow, Role role, string defaultDeviceId)
+        {
+            return;
+        }
+
+        public void OnPropertyValueChanged(string pwstrDeviceId, PropertyKey key)
+        {
+            return;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+
+namespace DeviceVolume
+{
+    static class Vault {
+        public static NAudio.CoreAudioApi.MMDeviceEnumerator deviceEnum = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+        public static string deviceName = "Moondrop Nekocake";
+        public static float defaultVolume = 0.2f;
+    }
+    static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            MMDevice defaultDevice = Vault.deviceEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            //while (true)
+            //{
+            //    int readableVolume = (int)(defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            //    Console.WriteLine($"{defaultDevice.DeviceFriendlyName} | {readableVolume}% => {(int)(Vault.defaultVolume * 100)}%");
+            //    defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar = Vault.defaultVolume;
+            //}
+            Console.WriteLine("---------- All devices ----------");
+            foreach (MMDevice device in Vault.deviceEnum.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active))
+            {
+                int readableVolume = (int)(device.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+                Console.WriteLine($"{readableVolume}% | {device.DeviceFriendlyName}");
+            }
+            Console.WriteLine("---------------------------------");
+            NAudioIMMNotificationClient client = new NAudioIMMNotificationClient();
+            Vault.deviceEnum.RegisterEndpointNotificationCallback(client);
+            await Task.Delay(-1);
+        }
+    }
+}
